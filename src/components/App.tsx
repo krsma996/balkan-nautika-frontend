dc647129bc102207fbaee87e806b911e9b9713f5
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-03-23 12:10:01 +0100

    pocetak dizajna i postavka
    pocetak dizajna i postavka aplikacije po nekim pravilima

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { HomePage } from "./homepage-components/HomePage";
+import { NavBarHeader } from "./homepage-components/homepage-presentation/navbar-header-components/NavBarHeader";
+import "../styles/global.css";
+import "../styles/tailwind.css";
+import "../styles/typografy.css";
+import "../styles/animations.css";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
