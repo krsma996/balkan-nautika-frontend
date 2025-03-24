3a46f58116fb848d3d524da0f0e4028ed4f54742
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-03-24 16:00:21 +0100

    dodaj carousel za biznis pravna lica
    dodat karousel za biznis pravna lica i test slike kasnije ce se ovo prilagodjavati

--- src/components/homepage-components/homepage-presentation/nautical-directory-carousel/NauticalBusinessDirectoryHub.tsx
+++ src/components/homepage-components/homepage-presentation/nautical-directory-carousel/NauticalBusinessDirectoryHub.tsx
@@ -1,10 +1,54 @@
+import { NauticalBusinessButton } from "./NauticalBusinessButtons";
 
 export const NauticalBusinessDirectoryHub: React.FC = () => {
   return (
     <div className="pt-18">
       <h1 className="pb-2 ml-30">Biznis prodavci i pravna lica</h1>
       <div className="p-10 bg-white shadow-lg rounded-lg">
-        <p>cao</p>
+        <div id="controls-carousel"className="relative w-full" data-carousel="static">
+          <div className="relative h-32 overflow-hidden rounded-lg md:h-64">
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="/docs/images/carousel/carousel-1.svg"
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <div
+              className="hidden duration-700 ease-in-out"
+              data-carousel-item="active"
+            >
+              <img
+                src="/docs/images/carousel/carousel-2.svg"
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="/docs/images/carousel/carousel-3.svg"
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="/docs/images/carousel/carousel-4.svg"
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="/docs/images/carousel/carousel-5.svg"
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+          </div>
+          <NauticalBusinessButton />
+        </div>
       </div>
     </div>
   );
