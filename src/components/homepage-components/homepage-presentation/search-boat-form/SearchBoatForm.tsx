c3a0c4ca22899411811990299d8547cdf23b03ea
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-03-24 10:57:00 +0100

    postavljena forma
    postavljena forma

--- src/components/homepage-components/homepage-presentation/search-boat-form/SearchBoatForm.tsx
+++ src/components/homepage-components/homepage-presentation/search-boat-form/SearchBoatForm.tsx
@@ -3,24 +3,26 @@ import { BoatFeaturesCheckBox } from "./search-boat-categories/BoatFeaturesCheck
 import { BoatMarks } from "./search-boat-categories/BoatMarks";
 import { BoatModels } from "./search-boat-categories/BoatModels";
 import { BoatRegion } from "./search-boat-categories/BoatRegion";
+import { BoatSearchButton } from "./search-boat-categories/BoatSearchButton";
 import { BoatType } from "./search-boat-categories/BoatType";
 import { BoatYear } from "./search-boat-categories/BoatYear";
 
 export const SearBoatForm: React.FC = () => {
     return (
-      <div className="pt-45 max-w-4x1 ml-30">
+      <div className="pt-45 max-w-4xl ml-30">
         <h1 className="pb-2">Pretraga plovila</h1>
-        <div className="p-10 bg-white shadow-lg roundend-lg">
+        <div className="p-10 bg-white shadow-lg rounded-lg">
           <form method="get" action="/nautika-oglasi/pretraga">
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-              <BoatMarks/>
-              <BoatModels/>
-              <input type="number" placeholder="cena"/>
-              <BoatYear/>
-              <BoatType/>
-              <BoatRegion/>
-              <BoatCondition/>
-              <BoatFeaturesCheckBox/>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+              <BoatMarks />
+              <BoatModels />
+              <input type="number" placeholder="cena" />
+              <BoatYear />
+              <BoatType />
+              <BoatRegion />
+              <BoatCondition />
+              <BoatFeaturesCheckBox />
+              <BoatSearchButton />
             </div>
           </form>
         </div>
