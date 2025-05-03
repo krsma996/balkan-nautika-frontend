9b01479b881161d48619c0acaf4c0843df0827ae
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2025-05-03 13:29:32 +0200

    Redizajn
    Redizajn navbar headera i ubacena font awesome iconica biblioteka

--- src/components/homepage-components/homepage-presentation/navbar-header/NavBarHeader.tsx
+++ src/components/homepage-components/homepage-presentation/navbar-header/NavBarHeader.tsx
@@ -1,7 +1,10 @@
 import { Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/react";
 import { XMarkIcon, Bars3Icon } from "@heroicons/react/16/solid";
-import logo from "../../../../img/logo.jpg";
+import logo from "../../../../img/logo.png";
 import { NavLinks } from "./NavLinks";
+import {faFacebook,faInstagram,faLinkedin,faYoutube} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export const NavBarHeader: React.FC = () => {
   return (
@@ -14,11 +17,26 @@ export const NavBarHeader: React.FC = () => {
               href="#"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
-              <img src={logo} className="h-8" alt="Flowbite Logo" />
+              <img src={logo} className="h-13 w-13" alt="Balkan Marina Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Balkan Nautika Hub
+                Balkan Marina
               </span>
             </a>
+            {/** SOCIAL MEDIA LINKS !!*/}
+            <div className="flex items-center space-x-5">
+              <a href="#" className="text-blue-600">
+                <FontAwesomeIcon icon={faFacebook} size="lg" />
+              </a>
+              <a href="#" className="text-pink-500">
+                <FontAwesomeIcon icon={faInstagram} size="lg" />
+              </a>
+              <a href="#" className="text-blue-800">
+                <FontAwesomeIcon icon={faLinkedin} size="lg" />
+              </a>
+              <a href="#" className="text-blue-800">
+                <FontAwesomeIcon icon={faYoutube} size="lg" />
+              </a>
+            </div>
 
             {/* Mobile menu button */}
             <DisclosureButton className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
