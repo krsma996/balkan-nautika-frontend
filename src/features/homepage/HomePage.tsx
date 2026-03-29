b7b955950ea79cfed09e0c18abf6b96dfc4be719
Nikola Krsmanovic <nikolakrsmanovic223@gmail.com>
2026-03-30 00:18:55 +0200

    minor promoene
    importi i folderi strukture fajlova promenjeni da bude logicno
    izbacena glupa slika logo i da se sredi UX za formu i to je to

--- src/features/homepage/HomePage.tsx
+++ src/features/homepage/HomePage.tsx
@@ -1,8 +1,8 @@
 import { Footer } from "./components/homepage-footer/Footer";
 //import { BoatGrid } from "./components/homepage-carousel-boats/BoatGrid";
-import { NauticalBusinessDirectoryHub } from "./components/nautical-directory-carousel/NauticalBusinessDirectoryHub";
-import { NavBarHeader } from "./components/navbar-header/NavBarHeader";
-import { SearBoatForm } from "./components/search-boat-form/SearchBoatForm";
+import { NauticalBusinessDirectoryHub } from "./components/homepage-nautical-directory-carousel/NauticalBusinessDirectoryHub";
+import { NavBarHeader } from "./components/homepage-navbar-header/NavBarHeader";
+import { SearBoatForm } from "./components/homepage-search-boat-form/SearchBoatForm";
 import { Activities } from "./components/homepage-activities/Activities";
 import { NavbarPrimaryMenu } from "./components/homepage-dropdown-navbar/NavbarPrimaryMenu ";
 import { NauticalCategories } from "./components/homepage-categories/NauticalCategories";
