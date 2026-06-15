ec7243da88f707507aa4d06fba554ba47b0eb1b8
Nikola Krsmanovic <nikolakrsmanovic223@gmail.com>
2026-06-15 17:24:56 +0200

    Dodate integracije
    Dodati keyloack settings , env varijable , AuthProvider komponenta koja radi sa keyloackom, instalirana keyloack biblioteka, i treba se namesti keyloack na neki dev server da bi netlify mogao da radi sa njim

--- src/features/homepage/components/homepage-navbar-header/NavLinks.tsx
+++ src/features/homepage/components/homepage-navbar-header/NavLinks.tsx
@@ -1,27 +1,45 @@
-const navLinksData = [
-  { navButton: "Prijavi se", href: "#" },
-  { navButton: "Registruj se", href: "#" },
-  { navButton: "POSTAVI OGLAS", href: "#" },
-];
+import { useAuth } from "../../../../config/AuthProvider";
 
 export const NavLinks = () => {
+  const { keycloak, authenticated } = useAuth();
+
+  const navLinksData = [
+    {
+      navButton: "Prijavi se",
+      onClick: () => keycloak.login(),
+    },
+    {
+      navButton: "Registruj se",
+      onClick: () => keycloak.register(),
+    },
+    {
+      navButton: "POSTAVI OGLAS",
+      onClick: () => {
+        if (authenticated) {
+          window.location.href = "#";
+        } else {
+          keycloak.login({
+            redirectUri: window.location.origin + "#",
+          });
+        }
+      },
+    },
+  ];
+
   return (
     <ul className="flex flex-col md:flex-row md:space-x-6 items-center">
       {navLinksData.map((item, index) => (
         <li key={index}>
-          <a
-            href={item.href}
-            className={`
-              text-sm font-medium transition
-              ${
-                item.navButton === "POSTAVI OGLAS"
-                  ? "bg-sky-500 text-white px-4 py-2 rounded-md hover:bg-sky-600"
-                  : "text-white/80 hover:text-white"
-              }
-            `}
+          <button
+            onClick={item.onClick}
+            className={`text-sm font-medium transition ${
+              item.navButton === "POSTAVI OGLAS"
+                ? "bg-sky-500 text-white px-4 py-2 rounded-md hover:bg-sky-600"
+                : "text-white/80 hover:text-white"
+            }`}
           >
             {item.navButton}
-          </a>
+          </button>
         </li>
       ))}
     </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './app/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app/App";
+import { AuthProvider } from "../src/config/AuthProvider";
 
 
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
